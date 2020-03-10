767ba5474eb71fd57cf1dedef71cffa60d276ad5
GoodField55 <goodfield055@gmail.com>
2020-03-10 07:23:31 +0000

    Modify travis_test_lightsensors,.py ( delete test_node_exist )

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -25,9 +25,9 @@ class LightsensorTest(unittest.TestCase):
 #    self.assertEqual(vs.sum_all, lf+ls+rs+rf, "different value: sum_all")
 #    self.assertEqual(vs.sum_forward, lf+rf, "different value: left_forard")
 
-  def test_node_exist(self):
-    nodes = rosnode.get_node_names()
-    self.assertIn('/lightsensors',nodes,"node does not exist")
+#  def test_node_exist(self):
+#    nodes = rosnode.get_node_names()
+#    self.assertIn('/lightsensors',nodes,"node does not exist")
 
 #  def test_get_value(self):
 #    rospy.set_param('lightsensors_freq',10)
